5f198f826ff58379b3e3b5581958a9161410760b
Hlupik77 <hlupik77@gmail.com>
2020-05-03 00:43:00 +0300

    Page_6/Footer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 $(function(){
-    $('.owl-carousel').owlCarousel({
+    $('.works-carousel').owlCarousel({
         loop:true,
         margin:30,
+        nav:true,
+        dots: false,
+        startPosition:1,
+        navText:['<img src="img/left.svg">', '<img src="img/right.svg">'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
+    $('.contributor-carousel').owlCarousel({
+        loop:true,
+        margin:0,
         nav:true,
         dots: false,
         startPosition:1,
